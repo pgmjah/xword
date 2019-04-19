4d9b66e92fd4f5c22b2fcf48bc88b8165aa972e0
Julian <pgmjah@gmail.com>
2019-04-19 10:59:21 -0400

    Changed .bigcircle to append jah-circle div

--- xword_worker.js
+++ xword_worker.js
@@ -1,11 +1,11 @@
-
 $("<div class='test-btn btn-convert-xword' title='Convert this puzzle to a blank puzzle, and print it.'>Convert To Blank Puzzle...</div>").prependTo(document.body).on("click", function()
 {
+	$("canvas").remove()
 	$(".letter").css("display", "none");
 	$("a").css("display", "none");
 	$(".subst, .subst2, .subst3, .subst4").css("display", "none");//subst classes are showing hints...hide them (went to four just in case...could be more?)
-	$(".bigcircle").removeClass("bigcircle");//.css({"backgroundPosition":"center", "backgroundSize":"90%", "backgroundImage":"none"});
-	
+	$(".bigcircle").append("<div class='jah-circle'>");
+
 	var puzTable = $(".PuzTable");
 	var clueBox = $(".cluebox").css({margin:"0px 0px 0px 20px", fontSize:"10px"});
 	var body = $("body").css("paddingTop", "10px").html("").append([puzTable, clueBox]);
